5a934c3be3b13387abe2df48df15fb8a63aff55f
Tom Oberhauser <tom@devfoo.de>
2015-01-07 21:40:16 +0100

    Delivery of static index.html is working

---
+++ src/ServerApplication.java
@@ -0,0 +1,48 @@
+import example.HelloResource;
+import example.Socket_on;
+import org.restlet.Application;
+import org.restlet.Component;
+import org.restlet.Restlet;
+import org.restlet.data.Protocol;
+import org.restlet.resource.Directory;
+import org.restlet.routing.Router;
+
+
+public class ServerApplication extends Application {
+	
+	
+	public static void main(String[] args) throws Exception {
+
+		// URI of the root directory.
+		final String ROOT_URI = "file:///Users/tom/Documents/Dev/Studium/vs/Restlet/Restlet/htdocs/";
+		
+		// Create a component
+		Component component = new Component();
+		component.getServers().add(Protocol.HTTP, 8112);
+		component.getClients().add(Protocol.FILE);
+		component.getClients().add(Protocol.HTTP);
+
+		// Create an application
+		Application application = new Application() {
+		    @Override
+		    public Restlet createInboundRoot() {
+		    	
+		    	// Create a router
+				Router router = new Router(getContext());
+
+				// Serve static HTML from ROOT_URI by default 
+				Directory directory = new Directory(getContext(), ROOT_URI);
+				router.attachDefault(directory);
+				
+				// Provide other functions depending on URL
+				router.attach("/on", Socket_on.class);
+				router.attach("/off", Socket_off.class);
+				return router;
+		    }
+		};
+
+		// Attach the application to the component and start it
+		component.getDefaultHost().attach(application);
+		component.start();
+	}
+}
---
+++ src/Socket_on.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import org.restlet.data.MediaType;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.resource.ClientResource;
+import org.restlet.resource.Get;
+import org.restlet.resource.ResourceException;
+import org.restlet.resource.ServerResource;
+
+public class Socket_on extends ServerResource{
+	@Get
+	public Representation my_get() throws ResourceException, IOException {
+
+		// Call the client resource
+		
+		ClientResource resource = new 
+		// ClientResource("http://192.168.178.31/event?port=4&action=1&pass=password");
+		ClientResource("http://www.ipv6lab.beuth-hochschule.de/event.php?port=3&action=1&pass=password");
+
+		Writer writer = new StringWriter();
+		resource.get().write(writer);
+
+		StringBuilder stringBuilder = new StringBuilder();
+
+		// Build the response as a HTML page
+		stringBuilder.append("<html>");
+		stringBuilder.append("<head><title>RESTful Application</title></head>");
+		stringBuilder.append("<body bgcolor=white>");
+		stringBuilder.append("<table border=\"0\">");
+		stringBuilder.append("<tr>");
+		stringBuilder.append("<td>");
+		stringBuilder.append("<h3>Status der Steckdose</h3>");
+		stringBuilder.append(writer.toString());
+		stringBuilder.append("</td>");
+		stringBuilder.append("</tr>");
+		stringBuilder.append("</table>");
+		stringBuilder.append("</body>");
+		stringBuilder.append("</html>");
+
+		return (new StringRepresentation(stringBuilder.toString(),
+				MediaType.TEXT_HTML));
+
+	}
+}
